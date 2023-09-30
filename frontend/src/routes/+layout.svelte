e4c8d5dd6ff11eb6558fb35509403d5f969c1847
Ajay Krishna K V <55079486+AJAYK-01@users.noreply.github.com>
2023-09-30 18:51:44 +0530

    move appbar to layout, and homebody component to homepage

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,25 @@
 <script>
+	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 </script>
+
+<!-- Fontawesome icons -->
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+/>
+
+<main>
+	<AppBar slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead">
+			<h2 class="h2">Frappe Library Management System</h2>
+		</svelte:fragment>
+		<svelte:fragment slot="trail">
+			<button type="button" class="btn rounded-full variant-filled-secondary">Add Book</button>
+			<button type="button" class="btn rounded-full variant-filled-secondary">Add Member</button>
+			<LightSwitch />
+		</svelte:fragment>
+	</AppBar>
+</main>
 
 <slot />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,29 +1,107 @@
 <script>
-	import { LightSwitch } from '@skeletonlabs/skeleton';
-	import { AppBar } from '@skeletonlabs/skeleton';
-	import HomeBody from '../components/HomeBody.svelte';
+	import { Drawer, initializeStores, getDrawerStore } from '@skeletonlabs/skeleton';
+
+	import IssueDrawerBody from '../components/IssueDrawerBody.svelte';
+	import ReturnDrawerBody from '../components/ReturnDrawerBody.svelte';
+	import SuggestedBooksBody from '../components/SuggestedBooksBody.svelte';
+	import SearchBody from '../components/SearchBody.svelte';
+
+	initializeStores();
+	const drawerStore = getDrawerStore();
+
+	function openIssueDrawer(book) {
+		const drawerSettings = {
+			id: 'issue',
+			position: 'right',
+			meta: book
+		};
+		drawerStore.open(drawerSettings);
+	}
+
+	function openReturnDrawer() {
+		const drawerSettings = {
+			id: 'return',
+			meta: { foo: 'bar', fizz: 'buzz', age: 40 }
+		};
+		drawerStore.open(drawerSettings);
+	}
+
+	let searchTitle = '';
+	let searchAuthor = '';
+	let searchToggle = false;
+	function onClearSearch() {
+		if (searchTitle == '' && searchAuthor === '') {
+			// refresh books from search result to home feed
+			searchToggle = false;
+		} else {
+			searchToggle = true;
+		}
+	}
 </script>
 
-<!-- Fontawesome icons -->
-<link
-	rel="stylesheet"
-	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-/>
-
 <main>
-	<AppBar slotDefault="place-self-center" slotTrail="place-content-end">
-		<svelte:fragment slot="lead">
-			<h2 class="h2">Frappe Library Management System</h2>
-		</svelte:fragment>
-		<svelte:fragment slot="trail">
-			<button type="button" class="btn rounded-full variant-filled-secondary">Add Book</button>
-			<button type="button" class="btn rounded-full variant-filled-secondary">Add Member</button>
-			<LightSwitch />
-		</svelte:fragment>
-	</AppBar>
 	<div class="container h-full mx-auto flex justify-center items-center">
 		<div class="space-y-5 m-5">
-			<HomeBody />
+			<div class="flex my-5 items-center justify-between">
+				<!-- Search by Title -->
+				<div
+					class="input-group input-group-divider rounded-full grid-cols-[auto_1fr_auto] w-1/4 h-full"
+				>
+					<div class="input-group-shim"><i class="fas fa-search" /></div>
+					<!-- Font Awesome search icon -->
+					<input
+						id="searchInput"
+						type="search"
+						class="px-3 py-2"
+						placeholder="Search by Book Title..."
+						bind:value={searchTitle}
+						on:input={onClearSearch}
+					/>
+				</div>
+
+				<!-- Spacer -->
+				<div class="flex-grow" />
+
+				<!-- Search by Authors -->
+				<div
+					class="input-group input-group-divider rounded-full grid-cols-[auto_1fr_auto] w-1/4 h-full"
+				>
+					<div class="input-group-shim"><i class="fas fa-search" /></div>
+					<!-- Font Awesome search icon -->
+					<input
+						id="searchInput"
+						type="search"
+						class="px-3 py-2"
+						placeholder="Search by Book Authors..."
+						bind:value={searchAuthor}
+						on:input={onClearSearch}
+					/>
+				</div>
+
+				<!-- Spacer -->
+				<div class="flex-grow" />
+
+				<!-- Return Book Button -->
+				<button
+					type="button"
+					class="btn rounded-full variant-filled-secondary ml-5 h-full"
+					on:click={openReturnDrawer}>Return Book</button
+				>
+			</div>
+
+			{#if searchToggle === true}
+				<SearchBody {searchTitle} {searchAuthor} {openIssueDrawer} />
+			{:else}
+				<SuggestedBooksBody {openIssueDrawer} />
+			{/if}
+
+			<Drawer width="200">
+				{#if $drawerStore.id === 'issue'}
+					<IssueDrawerBody />
+				{:else if $drawerStore.id === 'return'}
+					<ReturnDrawerBody />
+				{/if}
+			</Drawer>
 		</div>
 	</div>
 </main>
